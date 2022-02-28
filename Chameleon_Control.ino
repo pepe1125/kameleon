41c57b2ed20d0e56b6d3305023788fad86c2bd6b
Peter Sandor <pepe1125@gmail.com>
2022-02-28 21:06:26 +0100

    Update Chameleon_Control.ino

--- Chameleon_Control.ino
+++ Chameleon_Control.ino
@@ -2,26 +2,82 @@
 #include <DallasTemperature.h>
 #include <DS3231.h>
 #include <Wire.h>
-#include <TimeLib.h>
-
-DS3231 ora;
-
 OneWire oneWire(A2);
 DallasTemperature sensors(&oneWire);
 DeviceAddress insideThermometer;
+DS3231 Clock;
+byte Year;
+byte Month;
+byte Date;
+byte DoW;
+byte Hour;
+byte Minute;
+byte Second;
+bool Century = false;
+bool h12;
+bool PM;
+int interval = 1000;          // interval between sends
+long lastTime = 0;
 
 volatile uint16_t i = 2000;
 const uint16_t erosites = 3600;
 
 byte time_hour, time_min, old_time_min;
-
+float tempC;
 float tempC_old;
+byte uvcnt;
 bool disp;
-
 bool uv;
-bool century = false;
-bool h12Flag;
-bool pmFlag;
+bool reading;
+
+void GetDateStuff(byte& Year, byte& Month, byte& Day, byte& DoW,
+                  byte& Hour, byte& Minute, byte& Second) {
+  // Call this if you notice something coming in on
+  // the serial port. The stuff coming in should be in
+  // the order YYMMDDwHHMMSS, with an 'x' at the end.
+  boolean GotString = false;
+  char InChar;
+  byte Temp1, Temp2;
+  char InString[20];
+  byte j = 0;
+  while (!GotString) {
+    if (Serial.available()) {
+      InChar = Serial.read();
+      InString[j] = InChar;
+      j += 1;
+      if (InChar == 'x') {
+        GotString = true;
+      }
+    }
+  }
+  Serial.println(InString);
+  // Read Year first
+  Temp1 = (byte)InString[0] - 48;
+  Temp2 = (byte)InString[1] - 48;
+  Year = Temp1 * 10 + Temp2;
+  // now month
+  Temp1 = (byte)InString[2] - 48;
+  Temp2 = (byte)InString[3] - 48;
+  Month = Temp1 * 10 + Temp2;
+  // now date
+  Temp1 = (byte)InString[4] - 48;
+  Temp2 = (byte)InString[5] - 48;
+  Day = Temp1 * 10 + Temp2;
+  // now Day of Week
+  DoW = (byte)InString[6] - 48;
+  // now Hour
+  Temp1 = (byte)InString[7] - 48;
+  Temp2 = (byte)InString[8] - 48;
+  Hour = Temp1 * 10 + Temp2;
+  // now Minute
+  Temp1 = (byte)InString[9] - 48;
+  Temp2 = (byte)InString[10] - 48;
+  Minute = Temp1 * 10 + Temp2;
+  // now Second
+  Temp1 = (byte)InString[11] - 48;
+  Temp2 = (byte)InString[12] - 48;
+  Second = Temp1 * 10 + Temp2;
+}
 
 void setup() {
   Serial.begin(57600);
@@ -32,8 +88,6 @@ void setup() {
 
   pinMode(3, OUTPUT); // TRIAC   PORTD, 3
   pinMode(9, OUTPUT); // SSR     PORTB, 1
-
-  setTime(ora.getHour(h12Flag, pmFlag), ora.getMinute(), ora.getSecond(), ora.getDate(), ora.getMonth(century), ora.getYear());
 
   attachInterrupt(0, zero_cross_detect, RISING);
 }
@@ -46,41 +100,72 @@ void zero_cross_detect() {
 }
 
 void loop(void) {
-  while (Serial.available() > 1) {
+  if (Serial.available()) {
     byte c = Serial.read();
     if ((c == 49)) {
-      bitSet(PORTB, 1);
+      uv = 1;
       Serial.println("UV_ON");
     }
     else if ((c == 48)) {
-      bitClear(PORTB, 1);
+      uv = 0;
       Serial.println("UV_OFF");
     }
     else if ((c == 't')) {
-      Serial.print(year());
+      Serial.print(Clock.getYear(), DEC);
       Serial.print("-");
-      Serial.print(month());
+      Serial.print(Clock.getMonth(Century), DEC);
       Serial.print("-");
-      Serial.print(day());
+      Serial.print(Clock.getDate(), DEC);
       Serial.print(" ");
-      Serial.print(hour());
+      Serial.print(Clock.getHour(h12, PM), DEC); //24-hr
       Serial.print(":");
-      Serial.print(minute());
-      Serial.println();
+      Serial.print(Clock.getMinute(), DEC);
+      Serial.print(":");
+      Serial.println(Clock.getSecond(), DEC);
+    }
+    else if ((c == 'T')) {
+      GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
+      Clock.setClockMode(false);  // set to 24h
+      Clock.setSecond(Second);
+      Clock.setMinute(Minute);
+      Clock.setHour(Hour);
+      Clock.setDate(Date);
+      Clock.setMonth(Month);
+      Clock.setYear(Year);
+      Clock.setDoW(DoW);
+      Serial.println("OK");
     }
     else if ((c == 'i')) {
-      i = 4000;
+      byte value = Serial.read();
+      value = constrain(value, 49, 57);
+      value = value - 48;
+      i = 1000 * value;
       Serial.print(i);
       Serial.println();
     }
   }
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  float tempC = sensors.getTempC(insideThermometer);
-  if (tempC != tempC_old) {
-    tempC_old = tempC;
-    disp = true;
-    Serial.print(tempC);
-    Serial.print("C ");
+
+  if ((millis() - lastTime > interval)) { // REFRESH
+    reading = !reading;
+    if (reading) {
+      sensors.requestTemperatures(); // Send the command to get temperatures
+      tempC = sensors.getTempC(insideThermometer);
+      if (tempC != tempC_old) {
+        tempC_old = tempC;
+        disp = true;
+        Serial.print(tempC);
+        Serial.print("C ");
+      }
+    }
+    else {
+      time_hour = Clock.getHour(h12, PM);
+    }
+    uvcnt++;
+    if (uvcnt > 60) {
+      uv = 0;
+      uvcnt = 0;
+    }
+    lastTime = millis();
   }
 
   uint16_t futes;
@@ -88,16 +173,10 @@ void loop(void) {
     futes = erosites * (30.0 - tempC);
   else futes = 0;
 
-  time_hour = hour();
-  time_min = ora.getMinute();
-  if (time_min != old_time_min) {
-    setTime(ora.getHour(h12Flag, pmFlag), time_min, ora.getSecond(), ora.getDate(), ora.getMonth(century), ora.getYear());
-    old_time_min = time_min;
-    if (disp) Serial.print(" T ");
-  }
 
   if (time_hour >= 7 && time_hour < 8) {
-    if (i < 9500) i = i + 3;
+    futes = constrain(futes, 0, 9500);
+    if (i < futes && (millis() % 1000 == 0)) i = i + 5;
     if (disp) {
       Serial.print(i);
       Serial.println();
@@ -106,20 +185,40 @@ void loop(void) {
   else if (time_hour >= 8 && time_hour < 19) {
     i = constrain(futes, 0, 9500);
     if (disp) Serial.print(i);
-    if (tempC >= 27.0) {
+    if (time_hour >= 15)
+    {
+      if (uv) {
+        bitSet(PORTB, 1);
+      }
+      else {
+        bitClear(PORTB, 1);
+      }
+      if (disp) Serial.print(" UVt");
+    }
+    else if (tempC >= 27.0 && time_hour >= 11) {
       bitSet(PORTB, 1);
-      if (disp) Serial.print(" UV1");
+      if (disp) Serial.print(" UV>");
     }
     else if (tempC <= 26.0)
     {
-      bitClear(PORTB, 1);
-      if (disp) Serial.print(" UV0");
+      if (uv) {
+        bitSet(PORTB, 1);
+      }
+      else {
+        bitClear(PORTB, 1);
+      }
+      if (disp) Serial.print(" UV<");
     }
     if (disp) Serial.println();
   }
   else {
-    if (i > 2) i -= 2;
-    bitClear(PORTB, 1);
+    if (i > 2 && (millis() % 1000 == 0)) i -= 2;
+    if (uv) {
+      bitSet(PORTB, 1);
+    }
+    else {
+      bitClear(PORTB, 1);
+    }
     if (disp) {
       Serial.print(i);
       Serial.println();
